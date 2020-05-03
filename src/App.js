00bd9e65cd3bb4735a368b1979c1fe5a2db202ed
patmander007 <>
2020-05-03 20:38:36 +0800

    returned to class components

--- src/App.js
+++ src/App.js
@@ -3,49 +3,73 @@ import "./App.css";
 import Person from "./components/Person/Person";
 import cloneDeep from "lodash/cloneDeep";
 
-//functional components approach
-const App = (props) => {
-  const [personsState, setPersonsState] = useState({
+// class components approach
+class App extends Component {
+  state = {
     persons: [
       { name: "Patrick", age: 29 },
       { name: "Cherry", age: 26 },
       { name: "Paulala", age: 9 },
     ],
-  });
-
-  const switchNameHandler = () => {
-    setPersonsState({
-      persons: [
-        { name: "Cherry", age: 26 },
-        { name: "Patrick", age: 29 },
-        { name: "Paulalong", age: 9 },
-      ],
-    });
   };
 
-  return (
-    <div className="App">
-      <h1>Hello World! I am a react project!!</h1>
+  previousPersons = null;
 
-      <button onClick={switchNameHandler}>Switch Name</button>
+  switchNameHandler = () => {
+    console.log(this.previousPersons);
+    if (this.previousPersons == null) {
+      this.previousPersons = cloneDeep(personsState.persons);
+      this.setState({
+        persons: [
+          { name: "Cherry", age: 26 },
+          { name: "Patrick", age: 29 },
+          { name: "Paulalong", age: 9 },
+        ],
+      });
+    } else {
+      let tempPersons = cloneDeep(this.previousPersons);
+      this.previousPersons = cloneDeep(personsState.persons);
+      this.setState({
+        persons: tempPersons,
+      });
+    }
+  };
 
-      <Person
-        name={personsState.persons[0].name}
-        age={personsState.persons[0].age}
-      >
-        My hobbies are sleeping and sleeping again
-      </Person>
-      <Person
-        name={personsState.persons[1].name}
-        age={personsState.persons[1].age}
-      />
-      <Person
-        name={personsState.persons[2].name}
-        age={personsState.persons[2].age}
-      />
-    </div>
-  );
-};
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World! I am a react project!!</h1>
+
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+
+        <Person
+          name={personsState.persons[0].name}
+          age={personsState.persons[0].age}
+        >
+          My hobbies are sleeping and sleeping again
+        </Person>
+        <Person
+          name={personsState.persons[1].name}
+          age={personsState.persons[1].age}
+        />
+        <Person
+          name={personsState.persons[2].name}
+          age={personsState.persons[2].age}
+        />
+      </div>
+    );
+
+    // return React.createElement(
+    //   "div",
+    //   { className: "App" },
+    //   React.createElement(
+    //     "h1",
+    //     null,
+    //     "Hi, I'm a React app inside createElement"
+    //   )
+    // );
+  }
+}
 
 export default App;
 
@@ -57,72 +81,48 @@ export default App;
 //   );
 // }
 
-//class components approach
-// class App extends Component {
-//   state = {
+//functional components approach
+// const App = (props) => {
+//   const [personsState, setPersonsState] = useState({
 //     persons: [
 //       { name: "Patrick", age: 29 },
 //       { name: "Cherry", age: 26 },
 //       { name: "Paulala", age: 9 },
 //     ],
+//   });
+
+//   const switchNameHandler = () => {
+//     setPersonsState({
+//       persons: [
+//         { name: "Cherry", age: 26 },
+//         { name: "Patrick", age: 29 },
+//         { name: "Paulalong", age: 9 },
+//       ],
+//     });
 //   };
 
-//   previousPersons = null;
+//   return (
+//     <div className="App">
+//       <h1>Hello World! I am a react project!!</h1>
 
-//   switchNameHandler = () => {
-//     console.log(this.previousPersons);
-//     if (this.previousPersons == null) {
-//       this.previousPersons = cloneDeep(personsState.persons);
-//       this.setState({
-//         persons: [
-//           { name: "Cherry", age: 26 },
-//           { name: "Patrick", age: 29 },
-//           { name: "Paulalong", age: 9 },
-//         ],
-//       });
-//     } else {
-//       let tempPersons = cloneDeep(this.previousPersons);
-//       this.previousPersons = cloneDeep(personsState.persons);
-//       this.setState({
-//         persons: tempPersons,
-//       });
-//     }
-//   };
+//       <button onClick={switchNameHandler}>Switch Name</button>
 
-//   render() {
-//     return (
-//       <div className="App">
-//         <h1>Hello World! I am a react project!!</h1>
+//       <Person
+//         name={personsState.persons[0].name}
+//         age={personsState.persons[0].age}
+//       >
+//         My hobbies are sleeping and sleeping again
+//       </Person>
+//       <Person
+//         name={personsState.persons[1].name}
+//         age={personsState.persons[1].age}
+//       />
+//       <Person
+//         name={personsState.persons[2].name}
+//         age={personsState.persons[2].age}
+//       />
+//     </div>
+//   );
+// };
 
-//         <button onClick={this.switchNameHandler}>Switch Name</button>
-
-//         <Person
-//           name={personsState.persons[0].name}
-//           age={personsState.persons[0].age}
-//         >
-//           My hobbies are sleeping and sleeping again
-//         </Person>
-//         <Person
-//           name={personsState.persons[1].name}
-//           age={personsState.persons[1].age}
-//         />
-//         <Person
-//           name={personsState.persons[2].name}
-//           age={personsState.persons[2].age}
-//         />
-//       </div>
-//     );
-
-//     // return React.createElement(
-//     //   "div",
-//     //   { className: "App" },
-//     //   React.createElement(
-//     //     "h1",
-//     //     null,
-//     //     "Hi, I'm a React app inside createElement"
-//     //   )
-//     // );
-//   }
-// }
-
-// export default App;
+//export default App;
