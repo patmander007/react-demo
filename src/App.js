6c50fc721c8a0beb707c7092abd2d946f26889bf
patmander007 <>
2020-05-03 20:36:13 +0800

    functional component approach

--- src/App.js
+++ src/App.js
@@ -1,7 +1,53 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Person from "./components/Person/Person";
 import cloneDeep from "lodash/cloneDeep";
+
+//functional components approach
+const App = (props) => {
+  const [personsState, setPersonsState] = useState({
+    persons: [
+      { name: "Patrick", age: 29 },
+      { name: "Cherry", age: 26 },
+      { name: "Paulala", age: 9 },
+    ],
+  });
+
+  const switchNameHandler = () => {
+    setPersonsState({
+      persons: [
+        { name: "Cherry", age: 26 },
+        { name: "Patrick", age: 29 },
+        { name: "Paulalong", age: 9 },
+      ],
+    });
+  };
+
+  return (
+    <div className="App">
+      <h1>Hello World! I am a react project!!</h1>
+
+      <button onClick={switchNameHandler}>Switch Name</button>
+
+      <Person
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age}
+      >
+        My hobbies are sleeping and sleeping again
+      </Person>
+      <Person
+        name={personsState.persons[1].name}
+        age={personsState.persons[1].age}
+      />
+      <Person
+        name={personsState.persons[2].name}
+        age={personsState.persons[2].age}
+      />
+    </div>
+  );
+};
+
+export default App;
 
 // function App() {
 //   return (
@@ -11,71 +57,72 @@ import cloneDeep from "lodash/cloneDeep";
 //   );
 // }
 
-class App extends Component {
-  state = {
-    persons: [
-      { name: "Patrick", age: 29 },
-      { name: "Cherry", age: 26 },
-      { name: "Paulala", age: 9 },
-    ],
-  };
+//class components approach
+// class App extends Component {
+//   state = {
+//     persons: [
+//       { name: "Patrick", age: 29 },
+//       { name: "Cherry", age: 26 },
+//       { name: "Paulala", age: 9 },
+//     ],
+//   };
 
-  previousPersons = null;
+//   previousPersons = null;
 
-  switchNameHandler = () => {
-    console.log(this.previousPersons);
-    if (this.previousPersons == null) {
-      this.previousPersons = cloneDeep(this.state.persons);
-      this.setState({
-        persons: [
-          { name: "Cherry", age: 26 },
-          { name: "Patrick", age: 29 },
-          { name: "Paulalong", age: 9 },
-        ],
-      });
-    } else {
-      let tempPersons = cloneDeep(this.previousPersons);
-      this.previousPersons = cloneDeep(this.state.persons);
-      this.setState({
-        persons: tempPersons,
-      });
-    }
-  };
+//   switchNameHandler = () => {
+//     console.log(this.previousPersons);
+//     if (this.previousPersons == null) {
+//       this.previousPersons = cloneDeep(personsState.persons);
+//       this.setState({
+//         persons: [
+//           { name: "Cherry", age: 26 },
+//           { name: "Patrick", age: 29 },
+//           { name: "Paulalong", age: 9 },
+//         ],
+//       });
+//     } else {
+//       let tempPersons = cloneDeep(this.previousPersons);
+//       this.previousPersons = cloneDeep(personsState.persons);
+//       this.setState({
+//         persons: tempPersons,
+//       });
+//     }
+//   };
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello World! I am a react project!!</h1>
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>Hello World! I am a react project!!</h1>
 
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+//         <button onClick={this.switchNameHandler}>Switch Name</button>
 
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        >
-          My hobbies are sleeping and sleeping again
-        </Person>
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-        />
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        />
-      </div>
-    );
+//         <Person
+//           name={personsState.persons[0].name}
+//           age={personsState.persons[0].age}
+//         >
+//           My hobbies are sleeping and sleeping again
+//         </Person>
+//         <Person
+//           name={personsState.persons[1].name}
+//           age={personsState.persons[1].age}
+//         />
+//         <Person
+//           name={personsState.persons[2].name}
+//           age={personsState.persons[2].age}
+//         />
+//       </div>
+//     );
 
-    // return React.createElement(
-    //   "div",
-    //   { className: "App" },
-    //   React.createElement(
-    //     "h1",
-    //     null,
-    //     "Hi, I'm a React app inside createElement"
-    //   )
-    // );
-  }
-}
+//     // return React.createElement(
+//     //   "div",
+//     //   { className: "App" },
+//     //   React.createElement(
+//     //     "h1",
+//     //     null,
+//     //     "Hi, I'm a React app inside createElement"
+//     //   )
+//     // );
+//   }
+// }
 
-export default App;
+// export default App;
