d07d6f69b7143981a9f632e1d762cf30d3de90a5
patmander007 <>
2020-05-04 18:33:54 +0800

    person 2 way binding

--- src/App.js
+++ src/App.js
@@ -6,82 +6,57 @@ import cloneDeep from "lodash/cloneDeep";
 // class components approach
 class App extends Component {
   state = {
-    persons: [
-      { name: "Patrick", age: 29 },
-      { name: "Cherry", age: 26 },
-      { name: "Paulala", age: 9 },
-    ],
+    persons: [{ name: "Patrick", age: 29 }],
   };
-
-  previousPersons = null;
-
-  // switchNameHandler = () => {
-  //   if (this.previousPersons == null) {
-  //     this.previousPersons = cloneDeep(this.state.persons);
-  //     this.setState({
-  //       persons: [
-  //         { name: "Cherry", age: 26 },
-  //         { name: "Patrick", age: 29 },
-  //         { name: "Paulalong", age: 9 },
-  //       ],
-  //     });
-  //   } else {
-  //     let tempPersons = cloneDeep(this.previousPersons);
-  //     this.previousPersons = cloneDeep(this.state.persons);
-  //     this.setState({
-  //       persons: tempPersons,
-  //     });
-  //   }
-  // };
-
-  //overriding
-  // switchNameHandler = (newName) => {
-  //   this.setState({
-  //     persons: [
-  //       { name: "Cherry", age: 26 },
-  //       { name: newName, age: 29 },
-  //       { name: "Paulalong", age: 9 },
-  //     ],
-  //   });
-  // };
 
   switchNameHandler = (newName, newAge) => {
     this.setState({
-      persons: [
-        { name: "Cherry", age: 26 },
-        { name: newName, age: newAge },
-        { name: "Paulalong", age: 9 },
-      ],
+      persons: [{ name: newName, age: newAge }],
+    });
+  };
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [{ name: event.target.value, age: this.state.persons[0].age }],
+    });
+  };
+
+  ageChangedHandler = (event) => {
+    this.setState({
+      persons: [{ name: this.state.persons[0].name, age: event.target.value }],
     });
   };
 
   render() {
+    const buttonStyle = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "18px",
+      cursor: "pointer",
+    };
+
     return (
       <div className="App">
         <h1>Hello World! I am a react project!!</h1>
 
         {/* arrow function here is costly and not recommended */}
-        <button onClick={() => this.switchNameHandler("Norman", 25)}>
+        <button
+          style={buttonStyle}
+          onClick={() => this.switchNameHandler("Norman", 25)}
+        >
           Switch Name
         </button>
 
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
-        />
-
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
           clickMe={this.switchNameHandler.bind(this, "Patricio", 20)}
+          changedName={this.nameChangedHandler}
+          changedAge={this.ageChangedHandler}
         >
           My hobbies are sleeping and sleeping again
         </Person>
-
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        />
       </div>
     );
 
